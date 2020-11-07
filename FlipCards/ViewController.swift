29d54305b300f4d361c86e4f050e4cbd3f2b777d
Isakov Sultanbek Aibekovich <isakov_s@auca.kg>
2020-11-08 01:50:42 +0600

    flipcard

--- FlipCards/ViewController.swift
+++ FlipCards/ViewController.swift
@@ -14,6 +14,185 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Button11(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button12(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button13(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button14(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button21(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button22(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button23(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button24(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    @IBAction func Button31(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    @IBAction func Button32(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+  
+    
+    
+    @IBAction func Button33(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    
+    @IBAction func Button34(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button41(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button42(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    @IBAction func Button43(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func Button44(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+            sender.setTitle("👹", for: .normal)
+            sender.backgroundColor = .black
+        } else {
+            sender.setTitle("", for: .normal)
+            sender.backgroundColor = .black
+        }
+    }
+    
 }
+
+
+
+
 
